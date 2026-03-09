fec135aa3656266708b456b5b662f93590c41cd4
Nandini Monpara <nandinimonpara1@gmail.com>
2026-03-09 18:18:28 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -19,17 +19,12 @@ reporter: [
     }],
     ['blob', { outputDir: 'blob-report' }], // Use blob reporter
     ['json', { outputFile: './playwright-report/report.json' }],
-   /* ['@testdino/playwright', {
-       token:'trx_production_dcc2575f2b5be74215bb9cca642641d21ed572ca7056fa6c23d27ce853c070c0',
-       // token:'trx_development_33ce54f83ed9b2d937c78d180e2431a2ea83ed6e03a716219c7c6e90d853461a',
-      //token:'trx_staging_f3a3397c359d61b6d35bebc319d9720ed349f8c0fd9dcdbc41717453aea976a9',
-      //serverUrl: 'http://localhost:3000',
-      //serverUrl: 'https://staging-api.testdino.com',
-       serverUrl: 'https://api.testdino.com',
+    ['@testdino/playwright', {
+      token: process.env.TESTDINO_TOKEN,
       debug: true,
-      // uploadArtifacts: false,
+      serverUrl: 'https://api.testdino.com',
       
-    }], */
+    }], 
   ],
 
   use: {
